a7084c15b3ea5083d83ec1a82f2c957c8936d11c
foxfluff <foxfluff@users.noreply.github.com>
2016-06-18 17:00:56 -0400

    Minor tweaks and resolved edge case that was causing issues.  Added placeholder functions for later implementation

--- mandelbrot.py
+++ mandelbrot.py
@@ -6,25 +6,30 @@ class mandelbrot(object):
         self.cache = cache
 
     def calc_point(self, coordinate):
-        z, iterinit = cache_retr(coordinate)
+        z, iterinit = self.cache_retr(coordinate)
         # I sense that my future is full of grey hair and edge cases
-        c = complex(coordinate)
         for iteration in xrange(iterinit, self.iterations):
-            z = z**2 + c
+            z = z**2 + coordinate
             if z.real > 2: break
 
         self.cache_point(coordinate, iteration, z) #inb4 edge cases
-        if iteration == self.iterations and z.real < 2:
+        if iteration == self.iterations - 1 and z.real < 2:
             result = True
         else:
             result = False
+            #print 'false, %i' %iteration
 
         return result, iteration
 
-
     def cache_point(self, coordinate, iterations, value):
-        pass
+        if not self.cache:
+            return False
+        # actual caching implementation goes here :V
 
+    def cache_retr(self, coordinate):
+        # no cache exists yet sooooooooooo gonna just return as if nothing is
+        # cached
+        return complex(0, 0), 0
 
     # Properties
 
