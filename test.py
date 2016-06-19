bfa3f1affecd6b5b8bb03837d91e735409a72574
foxfluff <foxfluff@users.noreply.github.com>
2016-06-19 01:04:45 -0400

    Added custom characters to represent how quickly points escape the set

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import mandelbrot
+import math
 
-test_set = mandelbrot.mandelbrot()
+iterations = 50
+test_set = mandelbrot.mandelbrot(iterations)
 
 # just going to use console output, printing something like '#' for points in
 # the set.  Assuming default console size of 80x25 (typical for Windows
@@ -8,6 +10,10 @@ test_set = mandelbrot.mandelbrot()
 
 xmin, xmax = -2.0, 1.0
 ymin, ymax = -1.0, 1.0
+
+chars = ('#', ' ', '-', ';', '*')
+upper_bound = len(chars) - 1
+log_scale = math.log(iterations, upper_bound)
 
 width = 36 * 2  #correction for font not being square, mine happens to be 2:1
 height = 24 * 1
@@ -20,8 +26,12 @@ for y in range(height):
             ymax - y * (ymax - ymin) / height
             )
         #print real_x, real_y
-        if test_set.calc_point(complex(real_x, real_y))[0]:
-            row += "#"
+        in_set, iter = test_set.calc_point(complex(real_x, real_y))
+        if in_set:
+            row += chars[0]
         else:
-            row += " "
+            i = int(upper_bound
+                    * math.log(iter, upper_bound)
+                    / log_scale)
+            row += chars[i+1]
     print row
